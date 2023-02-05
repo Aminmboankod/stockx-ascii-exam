b47d9b0c7dd2a55fec0097efe256f88fa47598b9
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-02-05 23:39:54 +0100

    feat(lastSale): Add lastSale method and test

--- src/main/java/edu/craptocraft/stockx/criteria/LastSale.java
+++ src/main/java/edu/craptocraft/stockx/criteria/LastSale.java
@@ -1,16 +1,27 @@
 package edu.craptocraft.stockx.criteria;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import edu.craptocraft.stockx.item.Item;
 import edu.craptocraft.stockx.item.Offer;
+import edu.craptocraft.stockx.item.Sale;
 
 public class LastSale implements Criteria {
 
     @Override
     public List<Offer> checkCriteria(Item sneaker) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
+        List<Offer> lastSale = new ArrayList<>();
+        
+        for (Offer offer : sneaker.offers()) {
+            if (offer instanceof Sale) {
+                lastSale.add(offer);
+            }
+        }
+        
+        Offer last = lastSale.get(lastSale.size() - 1);
+        lastSale.clear();
+        lastSale.add(last);
+        return lastSale;
+      }
 }
--- src/test/java/edu/craptocraft/stockx/criteria/LastSaleTest.java
+++ src/test/java/edu/craptocraft/stockx/criteria/LastSaleTest.java
@@ -1,13 +1,31 @@
 package edu.craptocraft.stockx.criteria;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import edu.craptocraft.stockx.Stockx;
+import edu.craptocraft.stockx.item.Item;
+import edu.craptocraft.stockx.item.Offer;
+import edu.craptocraft.stockx.item.Sale;
+import edu.craptocraft.stockx.item.Sneaker;
 
 public class LastSaleTest {
     @Test
     public void constructorLastSaleTest() {
+        Item sneaker = new Sneaker("555088-105", "Jordan 1 Retro High Dark Mocha");
+        sneaker.add(new Sale("9.5", 352));
+        sneaker.add(new Sale("9.5", 404));
+        sneaker.add(new Sale("13", 360));
+        sneaker.add(new Sale("13", 372));
         Criteria lastSale = new LastSale();
         assertNotNull(lastSale);
+        List<Offer> actualSale = lastSale.checkCriteria(sneaker);
+        sneaker.setSale(actualSale.isEmpty()? 0 : actualSale.get(0).value());
+        System.out.println(Stockx.draw(sneaker));
+        assertEquals(372,lastSale.checkCriteria(sneaker).get(0).value());
     }
 }
